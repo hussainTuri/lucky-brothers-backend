4840aacace334d86fabbd1a4c7a1d468c7eb1ce7
Abrar Hussain <abrar.hussain@tiqqe.com>
2024-01-09 19:37:30 +0100

    Remove sharp package

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Node
 
-- use version 18. Sharp package was having issues running on Mac M2 machine when using node 16.
+- use version 18. Sharp package was having issues running on Mac M2 machine when using node 16. (Nops, can't use 18 on old ubuntu server so removed sharp for now)
   - The work around was:
   - `nvm use 18`
   - rm -rf node_modules/sharp
@@ -12,3 +12,4 @@
 ## Server
  - Installed nvm and using node v16.20.2 since it's a old version of ubuntu(14)
  - nvm is installed as webuser - NOT as root. Nvm is installed user specific 
+ - On each push, run `npm ci && npm run build` followed by `npm  start`
--- routes/products/uploadImage.ts
+++ routes/products/uploadImage.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { response } from '../../lib/response';
 import path from 'path';
-import sharp from 'sharp';
-import { userInfo } from 'os';
+// import sharp from 'sharp';
+// import { userInfo } from 'os';
 
 export const uploadImage = async (req: Request, res: Response) => {
   const resp = response();
@@ -15,28 +15,28 @@ export const uploadImage = async (req: Request, res: Response) => {
   }
 
   // let's create thumbnail too.
-  const ext = path.extname(file.originalname);
-  const resizedFileName = file.filename.replace(ext, '-300' + ext);
-  const resizedFilePath = path.join(file.destination, resizedFileName);
+  // const ext = path.extname(file.originalname);
+  // const resizedFileName = file.filename.replace(ext, '-300' + ext);
+  // const resizedFilePath = path.join(file.destination, resizedFileName);
 
   // Let's create thumbnail
-  await sharp(file.path)
-    .resize(300)
-    .toFormat('png')
-    .toFile(resizedFilePath)
-    .then((data) => {})
-    .catch((err) => {
-      console.error('Image upload error: ', err);
-      resp.success = false;
-      resp.message = 'Failed to upload image';
-      return res.status(500).json(resp);
-    });
+  // await sharp(file.path)
+  //   .resize(300)
+  //   .toFormat('png')
+  //   .toFile(resizedFilePath)
+  //   .then((data) => {})
+  //   .catch((err) => {
+  //     console.error('Image upload error: ', err);
+  //     resp.success = false;
+  //     resp.message = 'Failed to upload image';
+  //     return res.status(500).json(resp);
+  //   });
 
   resp.data = {
     path: path.join('uploads', file.filename),
     filename: file.filename,
     destination: file.destination,
-    thumbnail: path.join('uploads', resizedFileName),
+    // thumbnail: path.join('uploads', resizedFileName),
   };
 
   return res.json(resp);
