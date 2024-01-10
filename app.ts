3099a888cb13fd8918ebe75a2368761b18ccc171
Abrar Hussain <abrar.hussain@tiqqe.com>
2024-01-10 13:51:22 +0100

    Testing

--- app.ts
+++ app.ts
@@ -31,7 +31,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 app.use((req, res, next) => {
   const resp = response();
   resp.success = false;
-  resp.message = 'Route not found5!';
+  resp.message = 'Route not found6!';
 
   res.status(404).json(resp);
 });
