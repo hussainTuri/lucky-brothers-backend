2fd3fb032f6d2eb58710342b517ae8a7701d8768
Abrar Hussain <abrar.hussain@tiqqe.com>
2024-01-13 12:40:53 +0100

    Add cleanup script

---
+++ prisma/seed/clean.ts
@@ -0,0 +1,46 @@
+import { PrismaClient, PrismaPromise } from '@prisma/client';
+
+
+const prisma = new PrismaClient();
+
+// Should only be used in development
+const seedDatabase = async () => {
+  try {
+    console.log('Database cleaning started...');
+    const tables = [
+      'Product',
+      'PriceHistory',
+      'Customer',
+      'Invoice',
+      'InvoiceItem',
+      'InvoicePayment',
+    ];
+    const transactions: PrismaPromise<any>[] = []
+    transactions.push(prisma.$executeRaw`SET FOREIGN_KEY_CHECKS = 0;`)
+
+    for (const name of tables) {
+        try {
+          transactions.push(prisma.$executeRawUnsafe(`TRUNCATE ${name};`))
+        } catch (error) {
+          console.log({ error })
+        }
+    }
+
+    try {
+      await prisma.$transaction(transactions)
+      transactions.push(prisma.$executeRaw`SET FOREIGN_KEY_CHECKS = 1;`)
+    } catch (error) {
+      console.log({ error })
+    } finally {
+      transactions.push(prisma.$executeRaw`SET FOREIGN_KEY_CHECKS = 1;`)
+    }
+
+    console.log('Database cleaning successfully!');
+  } catch (error) {
+    console.error('Error cleaning database:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+seedDatabase();
