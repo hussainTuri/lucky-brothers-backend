1f814591e1a04031e97fff073d9fe9ec969e3a05
Abrar Hussain <abrar.hussain@tiqqe.com>
2024-01-15 19:29:24 +0100

    Comment out upload image copy

--- routes/products/uploadImage.ts
+++ routes/products/uploadImage.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { response } from '../../lib/response';
 import path from 'path';
 import sharp from 'sharp';
-import fs from 'fs';
 
 export const uploadImage = async (req: Request, res: Response) => {
   const resp = response();
@@ -33,13 +32,15 @@ export const uploadImage = async (req: Request, res: Response) => {
     });
 
   // copy the files to dist so that it's available in production
-  if (process.env.NODE_ENV === 'production') {
-    const distPath = path.join(process.env.FRONTEND_PATH!, 'dist', 'uploads');
-    console.log('distPath: ', distPath);
-    const distResizedFilePath = path.join(distPath, resizedFileName);
-    fs.copyFileSync(file.path, path.join(distPath, file.filename));
-    fs.copyFileSync(resizedFilePath, distResizedFilePath);
-  }
+  // NOT NEEDED ANYMORE AS I HAVE I HAVE ADDED NGINX CONFIGURATION TO SERVER THE IMAGES
+  // DIRECTLY FROM THE UPLOADS FOLDER
+  // if (process.env.NODE_ENV === 'production') {
+  //   const distPath = path.join(process.env.FRONTEND_PATH!, 'dist', 'uploads');
+  //   console.log('distPath: ', distPath);
+  //   const distResizedFilePath = path.join(distPath, resizedFileName);
+  //   fs.copyFileSync(file.path, path.join(distPath, file.filename));
+  //   fs.copyFileSync(resizedFilePath, distResizedFilePath);
+  // }
   resp.data = {
     path: path.join('uploads', file.filename),
     filename: file.filename,
