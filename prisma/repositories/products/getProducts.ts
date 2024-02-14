48519c1f7ecf622d58204bcaf20934a470bfcd16
Abrar Hussain <abrar.hussain@tiqqe.com>
2024-02-14 09:35:22 +0100

    Add isActive field to product table and filter results

--- prisma/repositories/products/getProducts.ts
+++ prisma/repositories/products/getProducts.ts
@@ -1,8 +1,11 @@
-import { Prisma, PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const getProducts = async () => {
   const products = await prisma.product.findMany({
+    where: {
+      isActive: true,
+    },
     include: {
       priceHistory: true,
     },
