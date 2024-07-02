dacc8a56c0997f0fec0fecc1035dab90238698c2
Abrar Hussain <abrar.hussain@postnord.com>
2024-07-02 20:01:08 +0200

    Add totalCustomersApproxLoanWithoutProfit in dashboard api

--- prisma/repositories/dashboard/getDashboard.ts
+++ prisma/repositories/dashboard/getDashboard.ts
@@ -38,9 +38,19 @@ export const getDashboard = async () => {
   );
   const totalExpenses = (expensesRs as any).length ? +(expensesRs as any)[0].totalExpenses : 0;
 
+  // sum of profit from pending invoices - this will help to exclude same amount being calculated toward total assets twice
+  // for example, we have stock value of 1000 and profit of 1000 and
+  // loan of 500. Loan will include part of profit as well. So we need to extract that.
+  const pendingProfitRs = await prisma.$queryRawUnsafe(
+    `SELECT sum(profit) AS pendingProfit FROM Invoice WHERE statusId = ${InvoiceStatusEnum.Pending}`,
+  );
+  const pendingProfit = (pendingProfitRs as any).length ? +(pendingProfitRs as any)[0].pendingProfit : 0;
+
+
   return {
     stockValue,
     totalCustomersLoan: loan,
+    totalCustomersApproxLoanWithoutProfit: pendingProfit,
     totalProfit: profit,
     totalExpenses,
   };
