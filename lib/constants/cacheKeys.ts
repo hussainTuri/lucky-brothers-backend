d3c6cb1972e0985920ceef5c032a64e0e8783732
Abrar Hussain <abrar.hussain@postnord.com>
2025-01-07 19:00:06 +0100

    Add cache for invoice related data

--- lib/constants/cacheKeys.ts
+++ lib/constants/cacheKeys.ts
@@ -1,3 +1,4 @@
 export const CacheKeys = {
   USERS: { key: 'users', ttl: 1440 }, // 24 hours
+  INVOICE_RELATED_DATA: { key: 'invoiceRelatedData', ttl: 1440 }, // 24 hours
 };
--- prisma/repositories/invoices/getRelatedData.ts
+++ prisma/repositories/invoices/getRelatedData.ts
@@ -1,10 +1,20 @@
-import { Prisma, PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { InvoiceRelatedData } from '../../../types/';
+import { getCache, setCache } from '../../../lib/utils';
+import { CacheKeys } from '../../../lib/constants';
 
 const prisma = new PrismaClient();
 
 export const getRelatedData = async () => {
+  const cachedData = getCache<InvoiceRelatedData>(CacheKeys.INVOICE_RELATED_DATA.key);
+  if (cachedData) {
+    return cachedData;  
+  }
   const data = {} as InvoiceRelatedData;
   data.statuses = await prisma.invoiceStatus.findMany();
+
+  // Cache data
+  setCache(CacheKeys.INVOICE_RELATED_DATA.key, data, CacheKeys.INVOICE_RELATED_DATA.ttl);
+
   return data;
 };
