7389f26ef801bab4ea44dd50d65323780d4b067c
Abrar Hussain <abrar.hussain@postnord.com>
2025-03-11 17:24:34 +0100

    Refactor save vehicle transaction (bank installment) to not affect the balance

--- prisma/repositories/transport/vehicles/transactions/saveTransaction.ts
+++ prisma/repositories/transport/vehicles/transactions/saveTransaction.ts
@@ -2,18 +2,26 @@ import { TransportVehicleTransaction } from '@prisma/client';
 import { OmitPrismaClient } from '../../../../../types';
 import { getCurrentBalance } from './common';
 import prisma from '../../../../../middleware/prisma';
+import { TransportVehicleTransactionTypes } from '../../../../../lib/enums';
 
 export const saveVehicleTransaction = async (
   entry: TransportVehicleTransaction,
 ): Promise<TransportVehicleTransaction | null> => {
   return prisma.$transaction(async (tx) => {
     const balance = await getCurrentBalance(entry.vehicleId, tx);
-    entry.balance = balance + entry.amount
+    entry.balance = balance + entry.amount;
+
+    if (entry.transactionTypeId === TransportVehicleTransactionTypes.BankInstallment) {
+      entry.balance = balance;
+    }
     return saveTransaction(entry, tx);
   });
 };
 
-export const saveTransaction = async (entry: TransportVehicleTransaction, tx: OmitPrismaClient): Promise<TransportVehicleTransaction|null> => {
+export const saveTransaction = async (
+  entry: TransportVehicleTransaction,
+  tx: OmitPrismaClient,
+): Promise<TransportVehicleTransaction | null> => {
   const entryCreated = await tx.transportVehicleTransaction.create({
     data: entry,
   });
