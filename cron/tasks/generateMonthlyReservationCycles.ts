a2d8460fb5f97c993d0630a5a3bc31f99b4e1865
Abrar Hussain <abrar.hussain@postnord.com>
2025-03-23 23:05:05 +0100

    Update generate montly cycles cron task

--- cron/tasks/generateMonthlyReservationCycles.ts
+++ cron/tasks/generateMonthlyReservationCycles.ts
@@ -4,49 +4,74 @@
  *
  */
 import prisma from '../../middleware/prisma';
-import { fixRentalCyclesDates, getReservationCyclesObjects } from '../utils/getReservationCycles';
+import { getReservationCyclesObjects } from '../utils/getReservationCycles';
 import {
   checkIfReservationCycleByMonthExists,
   saveVehicleReservationCycle,
 } from '../../prisma/repositories/transport';
-import { DateTime } from 'luxon';
-import { DUBAI_TZ } from '../../lib/constants';
 
-const runGenerateMonthlyReservationCyclesTask = async () => {
-  console.log(`[${new Date().toISOString()}] Running cron job...`);
+const runGenerateMonthlyReservationCyclesTask = async (dryRun = true) => {
+  console.log(`[${new Date().toISOString()}] Running cron job ... ${dryRun ? '(dry run)' : ''}`);
 
   try {
     // 1. Collect all reservations
-    // Get all reservations. filter old reservations where we have created a cycle that cover not just a month but
-    // all previous months or years.
-    const startOfMonth = DateTime.now().setZone(DUBAI_TZ).startOf('month').toJSDate(); // toJSDate() becomes UTC
-    const reservations = await prisma.transportVehicleReservation.findMany({
-      where: {
-        reservationStart: {
-          lt: startOfMonth,
-        },
-      },
-    });
+    const reservations = await prisma.transportVehicleReservation.findMany({});
 
     // 2. For reach reservation, build periods and map them to a cycle row object
+    const tableDataReservationToBeAdded = []; // cycles that wil be added at the end of this script execution
+    const tableDataReservationsAlreadyExisted = []; // This should be empty under normal circumstances
+    const tableDataReservationsWithoutCycles = []; // Reservation whose start and end dates doesn't generate any cycles anymore
     for (const reservation of reservations) {
+      const tableReservation = {} as any;
+      tableReservation.id = reservation.id;
+      tableReservation.customerId = reservation.customerId;
+      tableReservation.vehicleId = reservation.vehicleId;
+      tableReservation.reservationStart = reservation.reservationStart;
+      tableReservation.reservationEnd = reservation.reservationEnd;
+      tableReservation.monthlyRate = reservation.monthlyRate;
+
       const cycles = getReservationCyclesObjects(reservation);
       if (cycles.length === 0) {
-        console.log('No cycles found for reservation:', reservation.id);
+        // console.log('No cycles found for reservation:', reservation.id);
+        tableDataReservationsWithoutCycles.push({ ...tableReservation, CycleToAdd: 'None' });
         continue;
       }
 
       // 3. Check a cycle object against cycle table for its existence
       for (const cycle of cycles) {
+        const tableReservationCycle = {} as any;
+
         const exist = await checkIfReservationCycleByMonthExists(reservation.id, cycle.rentFrom);
         if (!exist) {
-          console.log('New cycle found', cycle);
+          // console.log('New cycle found', cycle);
+          tableReservationCycle.cycleRentFrom = cycle.rentFrom;
+          tableReservationCycle.cycleRentTo = cycle.rentTo;
+          tableReservationCycle.cycleAmount = cycle.amount;
 
           // 4 Add non existing periods to cycle table and rent to customer transactions
-          await saveVehicleReservationCycle(cycle);
+          if (!dryRun) {
+            await saveVehicleReservationCycle(cycle);
+          }
+          tableDataReservationToBeAdded.push({ ...tableReservation, ...tableReservationCycle });
+        } else {
+          tableDataReservationsAlreadyExisted.push({
+            ...tableReservation,
+            ...tableReservationCycle,
+            CycleAlreadyExisted: true,
+          });
         }
       }
     }
+
+    console.log('Following reservations have no cycles generated');
+    console.table(tableDataReservationsWithoutCycles);
+
+    console.log('Following cycles already existed');
+    console.table(tableDataReservationsAlreadyExisted);
+
+    console.log('Following cycles are added');
+    console.table(tableDataReservationToBeAdded);
+
     console.log(`[${new Date().toISOString()}] Done running cron job`);
   } catch (error) {
     console.error('Error running cron job:', error);
@@ -54,6 +79,4 @@ const runGenerateMonthlyReservationCyclesTask = async () => {
 };
 
 // Execute the task when the script runs
-// runGenerateMonthlyReservationCyclesTask();
-// fixReservationsDates();
-fixRentalCyclesDates();
+runGenerateMonthlyReservationCyclesTask(true);
--- cron/utils/getReservationCycles.ts
+++ cron/utils/getReservationCycles.ts
@@ -3,40 +3,43 @@ import {
   TransportVehicleReservationRentalCycle,
 } from '@prisma/client';
 import { DateTime } from 'luxon';
-import { DUBAI_TZ } from '../../lib/constants';
-import prisma from '../../middleware/prisma';
 
-// Let's keep things simple and use UAE timezone for all calculations but return UTC
-// UAE is +4 hours ahead of UTC
+// All the months data has been added manually up until Feb, 2025
+const includeMonthsAfterInclusive = '2025-03-01';
 // Prisma always stores dates in UTC format, that means we are only dealing UTC on backend
-
 export const getReservationCyclesObjects = (
   reservation: TransportVehicleReservation,
 ): TransportVehicleReservationRentalCycle[] => {
   if (!reservation?.id) return [];
   const MAX_CYCLES = 1000; // Safety stop to prevent infinite loops
 
-  // Convert reservation start & end to Dubai time
-  const reservationStart = DateTime.fromJSDate(reservation.reservationStart, { zone: 'utc' })
-    .setZone(DUBAI_TZ)
-    .startOf('day');
+  // Convert reservation start & end
+  const reservationStart = DateTime.fromJSDate(reservation.reservationStart, {
+    zone: 'utc',
+  }).startOf('day');
 
   const reservationEnd = reservation.reservationEnd
-    ? DateTime.fromJSDate(reservation.reservationEnd, { zone: 'utc' })
-        .setZone(DUBAI_TZ)
-        .endOf('day')
-    : DateTime.now().setZone(DUBAI_TZ).endOf('day');
+    ? DateTime.fromJSDate(reservation.reservationEnd, { zone: 'utc' }).endOf('day')
+    : DateTime.now().setZone('utc').endOf('day');
 
   let currentStart = reservationStart;
   const end = reservationEnd;
 
   const cycles: TransportVehicleReservationRentalCycle[] = [];
 
-  // console.log("Reservation:", reservation, { reservationStart: reservationStart.toJSDate(), reservationEnd });
+  // console.log('Reservation:', reservation, {
+  //   reservationStart: reservationStart.toJSDate(),
+  //   reservationEnd,
+  // });
 
   let cycleCount = 0;
   while (currentStart <= end && cycleCount < MAX_CYCLES) {
-    // Set period end at the last day of the current month in Dubai
+    // Bail if currentStart is in present month - cycle for present month will be generated at the start of next month
+    if (currentStart.year === DateTime.now().setZone('utc').year && currentStart.month >= DateTime.now().setZone('utc').month) {
+      break;
+    }
+
+    // Set period end at the last day of the current month
     const currentEnd = currentStart.endOf('month');
     const periodEnd = currentEnd > end ? end : currentEnd;
 
@@ -44,24 +47,23 @@ export const getReservationCyclesObjects = (
     const days = Math.round(periodEnd.diff(currentStart, 'days').days);
     const daysInMonth = currentStart.daysInMonth ?? 30;
 
-    // Convert Dubai time back to UTC for storage
-    const rentFromUtc = currentStart.setZone('utc').toJSDate();
-    const rentToUtc = periodEnd.setZone('utc').toJSDate();
-
     // Generate rental cycle entry
     const cycle = {
       vehicleReservationId: reservation.id,
       customerId: reservation.customerId,
-      rentFrom: rentFromUtc,
-      rentTo: rentToUtc,
+      rentFrom: currentStart.toJSDate(),
+      rentTo: periodEnd.toJSDate(),
       amount: Math.round(days * (reservation.monthlyRate / daysInMonth)),
     } as TransportVehicleReservationRentalCycle;
 
-    // console.log("Cycle:", { ...cycle, days, daysInMonth });
+    // console.log('Cycle:', { ...cycle, days, daysInMonth });
 
-    cycles.push(cycle);
+    // only include if currentStart is after Feb 2025 ie currentStart >= 2025-03-01
+    if (currentStart >= DateTime.fromISO(includeMonthsAfterInclusive, { zone: 'utc' })) {
+      cycles.push(cycle);
+    }
 
-    // Move to the first day of the next month in Dubai
+    // Move to the first day of the next month
     currentStart = currentStart.plus({ months: 1 }).startOf('month');
 
     cycleCount++; // Prevent infinite loops
@@ -69,90 +71,3 @@ export const getReservationCyclesObjects = (
 
   return cycles;
 };
-
-// no need to fix reservations dates, they seem correct
-// export const fixReservationsDates = async () => {
-//   const reservations = await prisma.transportVehicleReservation.findMany({
-//     where: {
-//       deleted: null,
-//     },
-//   });
-//   for (const reservation of reservations) {
-//     const reservationStart = DateTime.fromJSDate(reservation.reservationStart, { zone: 'utc' })
-//       .setZone(DUBAI_TZ);
-//       // .startOf('day'); // we want to preserve the user selected time
-//     const reservationEnd = reservation.reservationEnd
-//       ? DateTime.fromJSDate(reservation.reservationEnd, { zone: 'utc' })
-//           .setZone(DUBAI_TZ)
-//           //.endOf('day') // we want to preserve the user selected time
-//       : null;
-
-//     console.log(
-//       'update start date from: ',
-//       reservation.reservationStart,
-//       'to: ',
-//       reservationStart.toJSDate(),
-//     );
-//     console.log(
-//       'update end date from: ',
-//       reservation.reservationEnd,
-//       'to: ',
-//       reservationEnd?.toJSDate() ?? null,
-//     );
-//     // update reservation
-//     await prisma.transportVehicleReservation.update({
-//       where: { id: reservation.id },
-//       data: {
-//         reservationStart: reservationStart.toJSDate(),
-//         reservationEnd: reservationEnd?.toJSDate() ?? null,
-//       },
-//     });
-//   }
-// };
-
-export const fixRentalCyclesDates = async () => {
-  const reservations = await prisma.transportVehicleReservation.findMany({
-    where: {
-      deleted: null,
-    },
-    include: {
-      rentalCycles: {
-        where: {
-          deleted: null,
-        },
-      },
-    },
-  });
-  for (const reservation of reservations) {
-    for (const cycle of reservation.rentalCycles) {
-      // if(cycle.id !== 6) continue;
-      const rentFrom = DateTime.fromJSDate(cycle.rentFrom, { zone: 'utc' })
-        .plus({days: 1}) // add one day just to be sure we are not fucking up the data
-        .setZone(DUBAI_TZ)
-        .startOf('day');
-        // clone the date to avoid mutation
-      const rentTo = rentFrom.plus({}).endOf('month').endOf('day');
-
-      console.log(
-        cycle.id,
-        'update cycle start and end date from: ',
-        cycle.rentFrom,
-        'to: ',
-        rentFrom.toJSDate(),
-        ' ',
-        cycle.rentTo,
-        'to: ',
-        rentTo.toJSDate(),
-      );
-      // update cycle
-      await prisma.transportVehicleReservationRentalCycle.update({
-        where: { id: cycle.id },
-        data: {
-          rentFrom: rentFrom.toJSDate(),
-          rentTo: rentTo.toJSDate(),
-        },
-      });
-    }
-  }
-};
-//
